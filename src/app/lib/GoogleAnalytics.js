d6fbe9f9f8b61aa8461e676243936d61e9fe1195
David <dlc9113@gmail.com>
2024-05-28 13:04:14 +0800

    Fix GA script

--- src/app/lib/GoogleAnalytics.js
+++ src/app/lib/GoogleAnalytics.js
@@ -9,7 +9,7 @@ export default function GoogleAnalytics() {
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
       />
-      <Script>
+      <Script id='google-analytics'>
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
