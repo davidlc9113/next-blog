3126f6d282d216c7e878c7622fea824c2d20ab4e
David <dlc9113@gmail.com>
2024-06-04 18:59:37 +0800

    Add more button

--- src/app/ui/Content.jsx
+++ src/app/ui/Content.jsx
@@ -9,11 +9,14 @@ export default function Content() {
         href={`/articles/${e.path}`} 
         key={e.path} 
         className="flex flex-col justify-center items-center
-          p-4 my-4 mx-auto border shadow w-full h-full"
+          p-4 my-4 mx-auto w-full h-full border shadow"
       >
-        <h2 className=" text-center">{e.title}</h2>
-        <p className=" text-sm mb-2">{e.date}</p>
-        <p>{e.intro}</p>
+        <h2 className="text-center my-0">{e.title}</h2>
+        <p className="text-sm my-2">{e.date}</p>
+        <p className="my-2">{e.intro}</p>
+        <div className="text-sm my-0 py-2 px-4 border shadow rounded-lg">
+          Read more
+        </div>
       </Link>
     )
   });
